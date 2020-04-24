b1dbfc2335b356318b4c4fa616254bd5fde87ce4
KirtiKashyap <kashyap.kirti11@gmail.com>
2020-04-24 20:41:05 +0530

    remove unuse files

--- src/main/kotlin/com/user/restapi/user/repository/JournalRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.kotlinDemo.kotlinjournalapp.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import com.user.restapi.user.models.Journal
-import org.springframework.stereotype.Repository
-@Repository
-interface JournalRepository : JpaRepository<Journal, Long>{
-}
